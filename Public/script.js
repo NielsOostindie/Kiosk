3c195d53d1c70e71624aa4ee37a9de4764ee5598
NielsOostindie <112725677+NielsOostindie@users.noreply.github.com>
2024-04-16 22:25:56 +0200

    api done and working on demo

--- Public/script.js
+++ Public/script.js
@@ -1,15 +1,26 @@
 const items = [];
-const blacklist = [3, 4, 5, 6, 7, 8, 9, 10];
+var blacklist = [3, 4, 5, 6, 7, 8, 9, 10];
 const xhr = new XMLHttpRequest();
 let filter = 0;
-function buckets() {
-  filter = 1;
-}
-function burgers() {
-  filter = 2;
-}
-function drinks() {
-  filter = 3;
+
+function start() {
+  xhr.open("GET", "http://localhost/Kiosk/server/api.php?action=getCategories");
+  xhr.send();
+  xhr.responseType = "json";
+  xhr.onload = () => {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      console.log(xhr.response);
+      for (let i = 0; i < xhr.response.length; i++) {
+        var div = document.createElement("div");
+        items.push(div);
+        document.getElementById("grid-container").appendChild(div);
+        div.classList.add("grid-item");
+        div.innerHTML = `<img onclick="run(${xhr.response[i].ID})" id="${xhr.response[i].ID}" src="${xhr.response[i].Image}" alt="${xhr.response[i].Name}">`;
+      }
+    } else {
+      console.log(`Error: ${xhr.status}`);
+    }
+  };
 }
 
 function clear() {
@@ -18,25 +29,74 @@ function clear() {
   }
 }
 
-function test() {
+function test(ID, Meal) {
+  console.log(ID);
+  if (Meal == "1") {
+    // clear();
+    // var div = document.createElement("div");
+    // document.getElementById("grid-container").appendChild(div);
+    // div.classList.add("grid-item");
+    // div.innerHTML = `<img onclick='test(${xhr.response[0].ID});' id="${xhr.response[0].ID}" src="${xhr.response[0].Image}" alt="${xhr.response[0].Name}">`;
+    // var div = document.createElement("div");
+    // document.getElementById("grid-container").appendChild(div);
+    // div.classList.add("grid-item");
+    // div.innerHTML = `<img onclick='test(${xhr.response[2].ID});' id="${xhr.response[2].ID}" src="${xhr.response[2].Image}" alt="${xhr.response[2].Name}">`;
+    clear();
+    xhr.open(
+      "GET",
+      "http://localhost/Kiosk/server/api.php?action=getMealImages&product_id=" +
+        ID
+    );
+    xhr.send();
+    xhr.responseType = "json";
+    console.log(xhr.response);
+    xhr.onload = () => {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log(xhr.response);
+
+        var div = document.createElement("div");
+        items.push(div);
+        document.getElementById("grid-container").appendChild(div);
+        div.classList.add("grid-item");
+        div.innerHTML = `<img id="${xhr.response[0].ID}" src="${xhr.response[0].Image}">`;
+
+        var obj = xhr.response[0].MealImage;
+        var json = JSON.parse(obj);
+
+        for (let i = 1; i <= Object.keys(json).length; i++) {
+          var div = document.createElement("div");
+          items.push(div);
+          document.getElementById("grid-container").appendChild(div);
+          div.classList.add("grid-item");
+          div.innerHTML = `<img src="${json[i]}">`;
+        }
+      } else {
+        console.log(`Error: ${xhr.status}`);
+      }
+    };
+  } else {
+    console.log("false");
+  }
 }
 
-function run() {
+function run(id) {
   clear();
-  xhr.open("GET", "http://localhost/Kiosk/server/Api.php?filter=" + filter);
+  xhr.open(
+    "GET",
+    "http://localhost/Kiosk/server/api.php?action=getProducts&categorie=" + id
+  );
   xhr.send();
   xhr.responseType = "json";
   xhr.onload = () => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       console.log(xhr.response);
       for (let i = 0; i < xhr.response.length; i++) {
-        // if (Number(blacklist.includes(xhr.response[i].ID))) {
         if (blacklist.includes(Number(xhr.response[i].ID)) == false) {
           var div = document.createElement("div");
           items.push(div);
           document.getElementById("grid-container").appendChild(div);
           div.classList.add("grid-item");
-          div.innerHTML = `<img onclick='test();' id="${xhr.response[i].ID}" src="${xhr.response[i].Image}" alt="${xhr.response[i].Name}">`;
+          div.innerHTML = `<img onclick='test(${xhr.response[i].ID}, ${xhr.response[i].Meal});' id="${xhr.response[i].ID}" src="${xhr.response[i].Image}" alt="${xhr.response[i].Name}">`;
         }
       }
     } else {
--- server/Api.php
+++ server/Api.php
@@ -3,25 +3,99 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+function getCategories($conn)
+{
+	$sql = "SELECT * FROM categories";
+	$result = $conn->query($sql);
+
+	$data = array();
+
+	if ($result->num_rows > 0) {
+
+		while ($row = $result->fetch_assoc()) {
+			array_push($data, $row);
+		}
+		print json_encode($data);
+	}
+	$conn->close();
+}
+
+function getMealImages($conn)
+{
+	$product_id = $_GET['product_id'];
+
+	if (!is_numeric($product_id)) {
+		print("Invalid product id");
+		die();
+	}
+
+	$sql = "SELECT Image, MealImage FROM Products where ID = $product_id;";
+	$result = $conn->query($sql);
+
+	$data = array();
+
+	if ($result->num_rows > 0) {
+
+		while ($row = $result->fetch_assoc()) {
+			array_push($data, $row);
+		}
+		print json_encode($data);
+	}
+	$conn->close();
+}
+
+function getProducts($conn)
+{
+	$categorie = $_GET['categorie'];
+	
+	if (!is_numeric($categorie)) {
+		print("Invalid categorie id");
+		die();
+	}
+	$sql = "SELECT * FROM Products where Cat_ID = $categorie;";
+	$result = $conn->query($sql);
+
+	$data = array();
+
+	if ($result->num_rows > 0) {
+
+		while ($row = $result->fetch_assoc()) {
+			array_push($data, $row);
+		}
+		print json_encode($data);
+	}
+	$conn->close();
+}
+
 // Database connection
 $conn = new mysqli('localhost', 'root', '', 'Kiosk');
 if ($conn->connect_error) {
 	echo "$conn->connect_error";
 	die("Connection Failed : " . $conn->connect_error);
 } else {
-	$filter = $_GET['filter'];
-	$sql = "SELECT * FROM Products WHERE Cat_ID = $filter;";
-	$result = $conn->query($sql);
 
-	$data = array();
-
-	if ($result->num_rows > 0) {
-		
-		while ($row = $result->fetch_assoc()) {
-			array_push($data, $row);
-		}
-		echo json_encode($data);
-
+	if (!isset($_GET['action'])) {
+		print("No action provided");
+		die();
 	}
-	$conn->close();
+	$action = $_GET['action'];
+
+	$allowedActions = array('getCategories', 'getProducts', 'getMealImages');
+
+	if (!in_array($action, $allowedActions)) {
+		print("Invalid action");
+		die();
+	} else {
+		$action($conn);
+	}
+
+	// if ($action == 'getProducts' && isset($_GET['categorie'])) {
+	// 	getProducts($conn , $_GET['categorie']);
+	// } else if ($action == 'getCategories') {
+	// 	getCategories($conn);
+	// } else if ($action == 'getMealImages' && isset($_GET['product_id'])) {
+	// 	getMealImages($conn, $_GET['product_id']);
+	// }  else {
+	// 	print("Invalid action");
+	// }
 }
